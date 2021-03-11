28b4f2eebfaac9e75a502b415e386f178e9d8b8a
mrfrancej <france.jeffrey@GMAIL.COM>
2021-03-11 01:52:07 +0000

    first commit

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,11 +1,19 @@
+import { Contacts } from "../views/Contacts";
+
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
 			//Your data structures, A.K.A Entities
+			contatcs: []
 		},
 		actions: {
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
+			getContact: () => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/agenda/france_agenda")
+					.then(response => response.json())
+					.then(contacts => setStore({ contatcs: contacts }));
+			}
 		}
 	};
 };
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,17 +3,24 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
 export const Home = () => {
+    const [contactList, setContactList] = useState([]);
+    const getContactlist = () => {
+        fetch("https://assets.breatheco.de/apis/fake/contact/agenda/france_agenda")
+            .then(response => response.json())
+            .then(res => setContactList(res));
+    };
+    {
 
-		return (
-			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button, bootstrap is working
+        return (
+            <div className="text-center mt-5">
+                <h1>Hello Rigo!</h1>
+                <p>
+                    <img src={rigoImage} />
+                </p>
+                <a href="#" className="btn btn-success">
+                    If you see this green button, bootstrap is working
 				</a>
-			</div>
-		);
+            </div>
+        );
 
-}
+    }
